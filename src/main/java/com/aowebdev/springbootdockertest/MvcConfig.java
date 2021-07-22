c4fef99b93a7cd0e1233baf2da40644c64b07c0a
Alfonso Olivares <dream@Mac-mini-de-Alfonso.local>
2021-07-22 16:04:28 -0500

    No starting '/' for resource path

--- src/main/java/com/aowebdev/springbootdockertest/MvcConfig.java
+++ src/main/java/com/aowebdev/springbootdockertest/MvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("/application/BOOT-INF/classes/public/");
+        registry.addResourceHandler("public/**").addResourceLocations("classpath:public/");
     }
 }
