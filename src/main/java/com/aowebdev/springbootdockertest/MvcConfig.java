965a2268fdb6343729a799b2373f2ae8cfb1a137
Alfonso Olivares <dream@Mac-mini-de-Alfonso.local>
2021-07-22 13:24:39 -0500

    Changing static for public

--- src/main/java/com/aowebdev/springbootdockertest/MvcConfig.java
+++ src/main/java/com/aowebdev/springbootdockertest/MvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
     }
 }
