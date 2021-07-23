f5dae4746af76aec7b7f6160d4b3d2f8323bc578
Alfonso Olivares <dream@Mac-mini-de-Alfonso.local>
2021-07-23 10:14:28 -0500

    One to rule them all

--- src/main/java/com/aowebdev/springbootdockertest/config/MvcConfig.java
+++ src/main/java/com/aowebdev/springbootdockertest/config/MvcConfig.java
@@ -1,13 +1,21 @@
 package com.aowebdev.springbootdockertest.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "BOOT-INF/classes/public/",
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("BOOT-INF/classes/public/");
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
