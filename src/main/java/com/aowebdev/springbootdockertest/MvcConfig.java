a404e25c63d54ea4955084fa35d1153ffd6ea54f
Alfonso Olivares <dream@Mac-mini-de-Alfonso.local>
2021-07-22 19:33:25 -0500

    removing first slash after claspath

--- src/main/java/com/aowebdev/springbootdockertest/MvcConfig.java
+++ src/main/java/com/aowebdev/springbootdockertest/MvcConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
+            "classpath:META-INF/resources/", "classpath:resources/",
+            "classpath:static/", "classpath:public/" };
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
